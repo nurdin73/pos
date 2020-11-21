a4b11f7e4ee90f0cb26e198c73609ac4cec16c49
nurdin <nurdin.reverse73@gmail.com>
2020-11-21 15:00:13 +0700

    transaksi

--- resources/views/admin/report/transaksi.blade.php
+++ resources/views/admin/report/transaksi.blade.php
@@ -3,7 +3,52 @@
   <main class="c-main">
     <div class="container-fluid">
       <div class="fade-in">
-        
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <span class="lead">Laporan transaksi hari ini</span>
+                <button class="btn btn-sm btn-primary">Print</button>
+              </div>
+              <div class="card-body">
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <span class="lead">Laporan transaksi bulan ini</span>
+                <button class="btn btn-sm btn-primary">Print</button>
+              </div>
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <span class="lead">Laporan transaksi tahun ini</span>
+                <button class="btn btn-sm btn-primary">Print</button>
+              </div>
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <span class="lead">Laporan transaksi semua</span>
+                <button class="btn btn-sm btn-primary">Print</button>
+              </div>
+              <div class="card-body">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
