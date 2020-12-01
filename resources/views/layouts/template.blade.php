326afb528047abafe21e455590a6cf419ca31f7b
nurdin <nurdin.reverse73@gmail.com>
2020-12-01 17:30:05 +0700

    dark theme

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -42,6 +42,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" />
     @yield('css')
   </head>
+  {{-- <body class="c-app c-dark-theme"> --}}
   <body class="c-app">
     @include('components.sidebar')
 
