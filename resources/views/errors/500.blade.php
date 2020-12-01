00643cfe1ca5d2510642563fce0d8b15daf9fc38
nurdin <nurdin.reverse73@gmail.com>
2020-12-01 13:56:36 +0700

    error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v3.2.0
+* @link https://coreui.io
+* Copyright (c) 2020 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+<html lang="en">
+  <head>
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="Point of sale by nurdin rittercoding">
+    <meta name="author" content="nurdin">
+    <meta name="keyword" content="pos,point of sales, kasir">
+    <title>Error 500</title>
+    <!-- Main styles for this application-->
+    <link href="{{ asset('css/style.min.css') }}" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics-->
+  </head>
+  <body class="c-app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="clearfix">
+            <h1 class="float-left display-3 mr-4">500</h1>
+            <h4 class="pt-3">Maaf, Web sedang dalam masalah</h4>
+            <p class="text-muted">Halaman tidak dapat dimuat. Silahkan hubungi admin dari RitterCoding</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CoreUI and necessary plugins-->
+    <script src="{{ asset('vendors/@coreui/coreui/js/coreui.bundle.min.js') }}"></script>
+    <!--[if IE]><!-->
+    <script src="{{ asset('vendors/@coreui/icons/js/svgxuse.min.js') }}"></script>
+    <!--<![endif]-->
+
+  </body>
+</html>
