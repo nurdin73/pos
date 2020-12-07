d18d7dffffe6b3fec26a483dd95fc728ba239959
nurdin73 <nurdin.reverse73@gmail.com>
2020-12-07 21:55:21 +0700

    end section modal in pajak

--- resources/views/admin/managements/pajak/index.blade.php
+++ resources/views/admin/managements/pajak/index.blade.php
@@ -147,6 +147,7 @@
   </div>
   <!-- /.modal-dialog-->
 </div>
+@endsection
 
 @section('js')
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous"></script>
