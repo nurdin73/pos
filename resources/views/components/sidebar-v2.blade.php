4dcb76db322973ca96ab06933267794ff74fa6ca
nurdin73 <nurdin.reverse73@gmail.com>
2021-04-09 13:13:31 +0700

    update

--- resources/views/components/sidebar-v2.blade.php
+++ resources/views/components/sidebar-v2.blade.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Models\Menu;
-$menus = Menu::with(['subMenus' => function($q) {
-  $q->with(['childSubMenus' => function($z) {
-    $z->select('id', 'url', 'name', 'sub_menu_id');
-  }])->whereHas('roleAcceses', function($w) {
-    $w->where('role_id', auth()->user()->role_id)->where('isGranted', 1)->select('id', 'role_id', 'isGranted', 'sub_menu_id');
-  })->select('id', 'name', 'icon', 'url', 'menu_id');
-}])->get(['id', 'name']);
+use Illuminate\Support\Facades\Cache;
+
+$menus = Cache::rememberForever('menus:'. auth()->user()->role_id, function() {
+  return Menu::with(['subMenus' => function($q) {
+    $q->with(['childSubMenus' => function($z) {
+      $z->select('id', 'url', 'name', 'sub_menu_id');
+    }])->whereHas('roleAcceses', function($w) {
+      $w->where('role_id', auth()->user()->role_id)->where('isGranted', 1)->select('id', 'role_id', 'isGranted', 'sub_menu_id');
+    })->select('id', 'name', 'icon', 'url', 'menu_id');
+  }])->get(['id', 'name']);
+});
 ?>
 
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
